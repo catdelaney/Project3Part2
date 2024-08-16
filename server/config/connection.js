4cb6ae5906e46ded8c25e1dcfa608db210ec9de1
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-08-15 22:42:07 -0300

    connection edit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://golojhonalyn:Jhona101193@worldwidenews');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://golojhonalyn:Jhona101193@worldwidenews/worldwidenews');
 
 module.exports = mongoose.connection;
